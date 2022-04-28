88edb5135dea44eab41b89517f6982a026e2354d
Andermi <1210947553@qq.com>
2022-04-28 16:36:54 +0800

    v1.0.0

---
+++ pages/about.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import Button from '@mui/material/Button';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const TitleRed = styled(Button)`
+  color: red;
+  background: blue;
+`;
+
+// Use Title and Wrapper like any other React component – except they're styled!
+export default function App() {
+  return (
+    <div>
+      <Title>Hello World!</Title>
+      <TitleRed>Hello World!</TitleRed>
+      <TitleRed>Hello World!</TitleRed>
+      <Button>666</Button>
+    </div>
+  );
+}
